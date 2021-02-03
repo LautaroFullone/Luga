d198c88c4160fe7870299e4931ab0b641317ba75
LautaroFullone <lautarofullone@gmail.com>
2021-02-02 21:54:45 -0300

    Template

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class WebController extends Controller
 {
     public function index(){
-        return view('web.index-2');
+        return view('web.index');
     }
 }
