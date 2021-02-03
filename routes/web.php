af342d484c044cf9e83cefec9a90831c92bab038
LautaroFullone <lautarofullone@gmail.com>
2021-02-03 08:26:23 -0300

    Espaniol

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 //-------------------------------------------------------------------------------------
 
