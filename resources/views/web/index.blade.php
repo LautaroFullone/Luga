914778215a92e70bf15c7832704cec87f6cf1101
LautaroFullone <lautarofullone@gmail.com>
2021-02-19 09:26:26 -0300

    Nose que es

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -52,7 +52,8 @@
         <div class="body-wrapper">
             <!-- Begin Header Area -->
             <header>
-                <a href="https://api.whatsapp.com/send?phone=542233010313&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Luga-distribuidora%20." class="float" target="_blank">
+                <a href="https://api.whatsapp.com/send?phone=542233010313&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Luga-distribuidora%20."
+                class="float" target="_blank">
                     <i class="fa fa-whatsapp my-float"></i>
                 </a>
 
